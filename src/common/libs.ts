377089973489def737a83e7f05d3d423e5ad4997
Phil Cockfield <phil@cockfield.net>
2019-01-18 09:46:12 +1300

    Add YAML

--- src/common/libs.ts
+++ src/common/libs.ts
@@ -1,7 +1,7 @@
-export { value } from '@tdb/util/lib';
-
 import * as fs from 'fs-extra';
+import * as jsYaml from 'js-yaml';
 import * as fsPath from 'path';
 
-export { fs, fsPath };
+export { fs, fsPath, jsYaml };
+export { value } from '@tdb/util/lib';
 export { log } from '@tdb/log/lib/server';
