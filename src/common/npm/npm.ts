4303c36b8dcaff0a101c30b1e49998a2d7a0cd00
Phil Cockfield <phil@cockfield.net>
2019-01-22 08:54:31 +1300

    exports

--- src/common/npm/npm.ts
+++ src/common/npm/npm.ts
@@ -3,5 +3,6 @@ import { NpmPackage } from './NpmPackage';
 
 export { semver };
 export * from './get';
+export * from './NpmPackage';
 
 export const pkg = (dir?: string) => NpmPackage.create(dir);
