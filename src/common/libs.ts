3fe3a34666190a28b3fb3ffe3a396d6cfe043267
Phil Cockfield <phil@cockfield.net>
2019-01-24 20:54:05 +1300

    expose listr

--- src/common/libs.ts
+++ src/common/libs.ts
@@ -1,6 +1,7 @@
 import * as fsPath from 'path';
+import * as listr from 'listr';
 
-export { fsPath };
+export { fsPath, listr };
 export { R, value, time } from '@tdb/util/lib';
 export { log } from '@tdb/log/lib/server';
 export { isBinaryFile } from 'isbinaryfile';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { npm, fs, semver, NpmPackage, yaml, file } from './common';
+export { npm, fs, semver, NpmPackage, yaml, file, listr } from './common';
 export * from './types';
 export * from './tmpl';
